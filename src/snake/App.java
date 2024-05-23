baf69211ca23563f2ae3047fec49cb9f3811df7c
Lukas Matejovsky <lukyn.ma@gmail.com>
2024-05-23 15:29:18 +0200

    Začátek projektu

---
+++ src/snake/App.java
@@ -0,0 +1,10 @@
+package snake;
+
+import snake.global.LwjglWindow;
+
+public class App {
+    public static void main(String[] args) {
+        new LwjglWindow(new Renderer(), false);
+
+    }
+}
---
+++ src/snake/global/App.java
@@ -0,0 +1,9 @@
+package snake.global;
+
+public class App {
+
+    public static void main(String[] args) {
+        new LwjglWindow(new Renderer());
+    }
+
+}
